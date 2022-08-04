d1ed718064aaa3ddc41d5d24901ffbeb6c818bda
Maciej <maciek.m.stempniak@gmail.com>
2022-08-04 05:17:23 +0200

    YT Vid Downloader setup

---
+++ YouTube Video Downloader/README.md
@@ -0,0 +1,13 @@
+# YouTube Video Downloader
+
+## Requirements
+
+pytube `pip install pytube`
+
+## Usage
+
+`python download.py [yt link]`
+Example:
+`python download.py https://www.youtube.com/watch?v=dQw4w9WgXcQ`
+
+Video will be in "downloaded videos" folder
---
+++ YouTube Video Downloader/download.py
@@ -0,0 +1,16 @@
+from pytube import YouTube
+from sys import argv
+
+link = argv[1]
+yt = YouTube(link)
+
+print("Title: ", yt.title)
+print("Vievs: ", yt.views)
+
+yd = yt.streams.get_highest_resolution()
+
+print("Downloading... ")
+
+yd.download('downloaded videos')
+
+print("Downloaded")
